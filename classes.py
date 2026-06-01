43ba238bc8abb1c8745cb63f96c131df8771315a
Denival Biotto Filho <denivaldenival@gmail.com>
2026-06-01 09:09:23 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,3 +1,8 @@
 class Figurinha:
 
-    
+    def __init__(self, id, nome, pais, posicao, raridade):
+        self.id = id
+        self.nome = nome
+        self.pais = pais
+        self.posicao = posicao
+        self.raridade = raridade
