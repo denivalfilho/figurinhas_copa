dd0a2294fd91d5c2380c968c1ce020cef956eb72
Denival Biotto Filho <denivaldenival@gmail.com>
2026-06-03 17:24:14 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -27,3 +27,46 @@ class NodoFila:
     def __init__(self, item):
         self.item = item
         self.proximo = None
+
+class Fila:
+
+    def __init__(self):
+        self._inicio = None 
+        self._fim = None
+        self._tamanho = 0
+
+    def esta_vazia(self):
+        return self._inicio is None
+
+    def enqueue(self, item):
+        novo = NodoFila(item)
+        if self.esta_vazia():
+            self._inicio = novo
+            self._fim = novo
+        else:
+            self._fim.proximo = novo
+            self._fim = novo
+        self._tamanho += 1
+
+    def dequeue(self):
+        if self.esta_vazia():
+            return None
+        removido = self._inicio
+        self._inicio = self._inicio.proximo
+        if self._inicio is None:
+            self._fim = None
+        self._tamanho -= 1
+        return removido.item
+
+    def peek(self):
+        if self.esta_vazia():
+            return None
+        return self._inicio.item
+
+    def limpar(self):
+        self._inicio = None
+        self._fim = None
+        self._tamanho = 0
+
+    def tamanho(self):
+        return self._tamanho
